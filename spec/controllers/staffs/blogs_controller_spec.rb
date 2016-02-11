600dc205de6360326b85d1d5e1b83d1d1b837ffe
Junichi Ito <jit@sonicgarden.jp>
2016-02-11 10:30:26 +0900

    Fix comment

--- spec/controllers/staffs/blogs_controller_spec.rb
+++ spec/controllers/staffs/blogs_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-puts "staff/blogs_controller_spec.rb loaded"
+puts "staffs/blogs_controller_spec.rb loaded"
 RSpec.describe Staffs::BlogsController, type: :controller do
   let!(:blog) { create :blog, :draft }
   describe 'GET #index' do
