29433a066dc77d468a2385c0be7746e74fbe1e05
Junichi Ito <jit@sonicgarden.jp>
2016-02-11 11:00:16 +0900

    Refactoring

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,6 +1,4 @@
 class Staff < ActiveRecord::Base
   puts "Staff model loaded"
-  Dir[File.join(Rails.root, 'app/controllers/staff/*.rb')].each do |f|
-    require_dependency f
-  end
 end
+Dir[File.join(Rails.root, 'app/controllers/staff/*.rb')].each { |f| require_dependency f }
